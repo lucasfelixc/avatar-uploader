252c838f6e6781c484f9b08249436b7c974e54d3
Lucas Felix <lucasfelixdev@gmail.com>
2023-04-01 17:50:09 -0300

    test: crop slider test created

---
+++ __tests__/components/CropSlider/index.test.tsx
@@ -0,0 +1,52 @@
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { WrapperTheme } from '@/utils/test';
+import { CropSlider } from '@/components';
+
+describe('<CropSlider />', () => {
+  test('Should render', () => {
+    const { getByTestId } = WrapperTheme(
+      render,
+      <CropSlider handleChangeCropValue={() => null} handleClickSaveDimension={() => null} />,
+    );
+
+    expect(getByTestId('crop-content')).toBeInTheDocument();
+  });
+
+  test('Should change range input value', () => {
+    const alert = jest.spyOn(window, 'alert').mockImplementation(() => null);
+
+    const { getByTestId } = WrapperTheme(
+      render,
+      <CropSlider
+        handleChangeCropValue={(ev) => {
+          window.alert(ev);
+          console.log('pegada de cria');
+        }}
+        handleClickSaveDimension={() => null}
+      />,
+    );
+
+    const rangeInput = getByTestId('range-input');
+    fireEvent.change(rangeInput, { target: { value: 258 } });
+
+    expect(alert).toBeCalledWith('258');
+  });
+
+  test('Should click in save button', () => {
+    const alert = jest.spyOn(window, 'alert').mockImplementation(() => null);
+
+    const { getByTestId } = WrapperTheme(
+      render,
+      <CropSlider
+        handleChangeCropValue={() => null}
+        handleClickSaveDimension={() => window.alert('Save button was clicked')}
+      />,
+    );
+
+    const saveButton = getByTestId('save-button');
+    fireEvent.click(saveButton);
+
+    expect(alert).toBeCalledWith('Save button was clicked');
+  });
+});
