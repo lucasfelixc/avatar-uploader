56b42fb1386003fcf76c93f6deef7aecf70ab9b3
Lucas Felix <lucasfelixdev@gmail.com>
2023-04-03 12:33:19 -0300

    chore: image context created

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { ImageProvider } from '@/context/ImageContext';
 import { ThemeProvider } from '@/styles/ThemeProvider';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -11,9 +12,11 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
         <link rel='shortcut icon' href='/img/icon-branding.webp' />
       </Head>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ImageProvider>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ImageProvider>
     </>
   );
 };
